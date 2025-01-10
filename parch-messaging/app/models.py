6deed3ccd68f1b30843b47d6642b1840b7bb890a
rahilisashaik <rahilisashaik@berkeley.edu>
2025-01-10 17:20:53 -0500

    django server initialization

---
+++ parch-messaging/app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class File(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    url = models.TextField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    file_id = models.ForeignKey(File, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+    
+
---
+++ parch-messaging/app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+from .models import File, Comment
+
+class FileSerializer(ModelSerializer):
+    class Meta:
+        model = File
+        fields = "__all__"
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
